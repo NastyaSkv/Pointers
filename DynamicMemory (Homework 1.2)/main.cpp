62befe7bd4f8e58eb19b46ac9e8aa4ed7f232df3
NastyaSkv <NastyaSkv@DESKTOP-S0J6MVO>
2022-09-13 02:00:32 +0300

    homework

--- DynamicMemory (Homework 1.2)/main.cpp
+++ DynamicMemory (Homework 1.2)/main.cpp
@@ -11,6 +11,10 @@ void FillRand(int* arr, const int n);
 void Print(int* arr, const int n);     
 int* push_back(int* arr, int& n, int value);
 int* push_front(int* arr, int& n, int value);
+int* insert(int* arr, int& n, int value, int index);
+int* pop_back(int* arr, int& n);
+int* pop_front(int* arr, int& n);
+int* erase(int* arr, int& n, int index);
 
 void main()
 {
@@ -33,8 +37,22 @@ void main()
 	Print(arr, n);
 
 	cout << "Введите индекс добавляемого значения: "; cin >> index;
-	cout << "Введите добавляемое значение: "; cin >> value;
-	
+	cout << endl << "Введите добавляемое значение: "; cin >> value;
+	arr = insert(arr, n, value, index);
+	Print(arr, n);
+
+	cout << "Удалим значение в конце: " << endl;
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	cout << "Удалим значение в начале: " << endl;
+	arr = pop_front(arr, n);
+	Print(arr, n);
+
+	cout << "Введите индекс удаляемого значения: " << endl; cin >> index;
+	arr = erase(arr, n, index);
+	Print(arr, n);
+
 	delete[] arr;
 }
 
@@ -52,7 +70,7 @@ void Print(int* arr, const int n)
 	{
 		cout << arr[i] << "\t";
 	}
-	cout << endl;
+	cout << endl<<endl;
 }
 
 int* push_back(int* arr, int& n, int value)
@@ -84,7 +102,6 @@ int* push_front(int* arr, int& n, int value)
 	delete[] arr;
 	arr = buffer; 	                            //3) Подменяем адрес исх массива в указателе 'arr' адресом нового 
 	arr[0] = value;
-
 	n++;
 	return(arr);
 }
@@ -92,16 +109,59 @@ int* push_front(int* arr, int& n, int value)
 int* insert(int* arr, int& n, int value, int index)
 {
 	int* buffer = new int[n + 1];               //1) Создаем буферный массив нужного размера
-
-	for (int i = 1; i <= n; i++)                 //2) Копируем содержимое исходного массива в буферный:
+	int j = 0;
+	for (int i = 0; i <= n; i++)                 //2) Копируем содержимое исходного массива в буферный:
 	{
-		buffer[i] = arr[i - 1];
+		(i == index) ? buffer[i] = value : (buffer[i] = arr[j], j++);
 	}
 
 	delete[] arr;
 	arr = buffer; 	                            //3) Подменяем адрес исх массива в указателе 'arr' адресом нового 
-	arr[0] = value;
-
 	n++;
+	return(arr);
+}
+
+int* pop_back(int* arr, int& n)
+{
+	int* buffer = new int[n - 1];               
+
+	for (int i = 0; i < n-1; i++)                
+	{
+		buffer[i] = arr[i];
+	}
+
+	delete[] arr;
+	arr = buffer; 	                          
+	n--;
+	return(arr);
+}
+
+int* pop_front(int* arr, int& n)
+{
+	int* buffer = new int[n - 1];               
+
+	for (int i = 0; i < n; i++)              
+	{
+		buffer[i] = arr[i+1];
+	}
+
+	delete[] arr;
+	arr = buffer; 	                            
+	n--;
+	return(arr);
+}
+
+int* erase(int* arr, int& n, int index)
+{
+	int* buffer = new int[n - 1];               //1) Создаем буферный массив нужного размера
+	int i=0, j = 0;
+	for (; i < n; i++, j++)                 //2) Копируем содержимое исходного массива в буферный:
+	{
+		(i == index) ? buffer[i] = arr[++j] : buffer[i] = arr[j];
+	}
+
+	delete[] arr;
+	arr = buffer; 	                            //3) Подменяем адрес исх массива в указателе 'arr' адресом нового 
+	n--;
 	return(arr);
 }
